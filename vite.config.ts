5f42aa0ca47715f2c46224a594d533f35296c7ee
Ahmed Oueslati <99073363+AhmedOueslatii@users.noreply.github.com>
2025-07-28 17:01:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/strat-ai-plan-84-main/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
